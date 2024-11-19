09f1d6dacc340542b15c3e1d2ce793b4cbb3f8d7
bowostucel <wibowo@stucel.com>
2024-11-19 12:50:16 +0700

    add home

---
+++ page-templates/page-home.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Template Name: Home
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage 
+ * @since 1.0
+ * @version 1.0
+ */
+
+get_header();
+
+// code here
+
+get_footer();
