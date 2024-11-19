7ac3c1ee470179cece4892fc02252bddf49ebb99
bowostucel <wibowo@stucel.com>
2024-11-19 12:28:51 +0700

    first commit

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package s_
+ */
+
+/**
+ * Functions which enhance the theme by hooking into WordPress.
+ */
+
+
---
+++ header.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * The header for our theme
+ *
+ * @package _s
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title><?php wp_title(''); ?></title>
+    <?php wp_head(); ?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <header id="header" class="header">
+        
+    </header>
+    <main>
